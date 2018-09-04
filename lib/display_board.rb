bdabfeaf664920a83e14d3ec3d288c5aeee6cf18
Richard Dinwiddie <rdinwiddie3@gmail.com>
2018-09-04 15:05:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,18 +1,11 @@
 board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 
+
+
 def display_board(board)
-  puts "   ","|","   ","|","   "
-  puts -----------
-  puts "   ","|","   ","|","   "
-  puts -----------
-  puts "   ","|","   ","|","   "
-
-
-
-def display_board(board2)
-  puts "#{board0}","|","#{board1}","|","#{baord2}"
+  puts "#{board0},|,#{board1},|,#{baord2}"
   puts "-----------"
-  puts "#{board3}","|","board4","|","#{board5}"
+  puts "#{board3},|,board4,|,#{board5}"
   puts "-----------"
-  puts "#{board6}","|","#{board7}","|","board7"
+  puts "#{board6},|,#{board7},|,board7"
   end
